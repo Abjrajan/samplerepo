b319f1b2886acb6eb0b1dcda4483654fcee97e6a
User <User@DESKTOP-EAMD381>
2024-11-15 10:22:54 +0530

    initial commit

---
+++ samplemaven/src/main/java/samplemavenpackage1/Default.java
@@ -0,0 +1,11 @@
+package samplemavenpackage1;
+
+public class Default {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Hello World";
+System.out.println(s);
+	}
+
+}
